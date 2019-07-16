da107154f7258ed03edd59b9388ff48df61127fb
ivpizzi <33039021+ivpizzi@users.noreply.github.com>
2019-07-16 15:54:46 -0400

    Add files via upload

--- Trashket Ball/Trashket Ball/GameViewController.swift
+++ Trashket Ball/Trashket Ball/GameViewController.swift
@@ -17,37 +17,16 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            
+            let scene = GameScene(size: view.bounds.size)
+            view.presentScene(scene)
+            scene.scaleMode = .aspectFill
+            
             
             view.ignoresSiblingOrder = true
             
             view.showsFPS = true
             view.showsNodeCount = true
+            view.showsPhysics = true
         }
-    }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-    
-    
-    
-}
+    }}
